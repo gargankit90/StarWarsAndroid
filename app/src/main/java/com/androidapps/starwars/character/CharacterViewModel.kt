8ec6accedfa2f1f5b69476f56d8c5c95b8250ee2
Ankit Garg <gargankit90@gmail.com>
2018-08-12 13:58:06 -0700

    Using composite disposables to clear RxObservers.

--- app/src/main/java/com/androidapps/starwars/character/CharacterViewModel.kt
+++ app/src/main/java/com/androidapps/starwars/character/CharacterViewModel.kt
@@ -5,6 +5,8 @@ import android.arch.lifecycle.ViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
+import io.reactivex.disposables.CompositeDisposable
+
 
 /**
  * Created by ankit on 8/11/18.
@@ -15,15 +17,21 @@ class CharacterViewModel @Inject constructor(
         val characterRepository: CharacterRepository) : ViewModel() {
 
     var charactersLiveData: MutableLiveData<List<Character>> = MutableLiveData()
+    private val disposables = CompositeDisposable()
 
-    // TODO: Add composite disposables and clear the observables when viewmodel is cleared
     fun loadCharacter(): MutableLiveData<List<Character>> {
-        characterRepository.loadCharacters()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe() {
-                    charactersLiveData.value = it.characterList
-                }
+        disposables.add(
+                characterRepository.loadCharacters()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe() {
+                            charactersLiveData.value = it.characterList
+                        })
         return charactersLiveData
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
     }
 }
